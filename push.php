9ab0324b160357d1ce335474058e2b4d90f33c96
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-05-13 19:11:56 +0530

    Added DB creds

--- push.php
+++ push.php
@@ -1,7 +1,13 @@
 <?php
+<<<<<<< Updated upstream
 $servername = "svcehost";
 $username = "root";
 $password = "";
+=======
+$servername = "localhost";
+$username = "svcehost_DJ2_0";
+$password = "Svceacm123";
+>>>>>>> Stashed changes
 $dbname = "svcehost_DJ2.0";
 $flag=true;
 $id = "\"".$_POST['gh_id']."\"";
@@ -15,7 +21,11 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+<<<<<<< Updated upstream
 $check_sql = "select count(1) as \"check\" from registration where email=".$email.";";
+=======
+$check_sql = "select count(1) as \"check\" from Participant_List where email=".$email.";";
+>>>>>>> Stashed changes
 //$check_stmt = $conn->prepare($check_sql);
 //$check_stmt->execute();
 $result = $conn->query($check_sql);
